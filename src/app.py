7b7d1fdcdaad2d20e630df60b46f4514862c10f8
Mikhail <kozlov_m2@dogma.ru>
2024-05-14 10:17:44 +0300

    users
    api is implemented

--- src/app.py
+++ src/app.py
@@ -4,9 +4,8 @@ import sys
 sys.path.append(os.path.dirname(__file__))
 
 from litestar import Litestar
-from litestar.contrib.sqlalchemy.plugins import SQLAlchemyInitPlugin
 
-from .db import on_startup, sqlalchemy_config
+from .db import on_startup, sqlalchemy_plugin
 
 from .organization.controllers import CityController
 from .users.controllers import UserController
@@ -17,6 +16,6 @@ app = Litestar(
         UserController,
     ],
     on_startup=[on_startup],
-    plugins=[SQLAlchemyInitPlugin(config=sqlalchemy_config)],
+    plugins=[sqlalchemy_plugin],
     debug=True,
 )
--- src/users/controllers.py
+++ src/users/controllers.py
@@ -1,9 +1,9 @@
-from uuid import UUID, uuid4
+from uuid import UUID
 from litestar import Controller, get, post, delete
 from litestar.di import Provide
 from pydantic import TypeAdapter
 
-from users.models import UserRepository, provide_user_repository
+from users.models import UserModel, UserRepository, provide_user_repository
 from users.scheme import UserReadSchema, UserCreateSchema, UserUpdateSchema
 
 
@@ -13,22 +13,34 @@ class UserController(Controller):
 
     @get("/")
     async def get_list(self, users_repo: UserRepository) -> list[UserReadSchema]:
-        result = await users_repo.list()
+        objs = await users_repo.list()
         type_adapter = TypeAdapter(list[UserReadSchema])
-        return type_adapter.validate_python(result)
+        return type_adapter.validate_python(objs)
 
     @get("/{user_id:uuid}")
-    async def get(self, user_id: UUID) -> UserReadSchema:
-        return UserReadSchema(id=uuid4(), email="user1@mail.ru", username="user1")
+    async def get(self, user_id: UUID, users_repo: UserRepository) -> UserReadSchema:
+        obj = await users_repo.get(user_id)
+        return UserReadSchema.model_validate(obj)
 
     @post("/")
-    async def create(self, data: UserCreateSchema) -> UserReadSchema:
-        return UserReadSchema(id=uuid4(), email="user1@mail.ru", username="user1")
+    async def create(
+        self, data: UserCreateSchema, users_repo: UserRepository
+    ) -> UserReadSchema:
+        obj = await users_repo.add(UserModel(**data.model_dump(exclude_unset=True)))
+        await users_repo.session.commit()
+        return UserReadSchema.model_validate(obj)
 
     @post("/{user_id:uuid}")
-    async def update(self, user_id: UUID, data: UserUpdateSchema) -> UserReadSchema:
-        return UserReadSchema(id=uuid4(), email="user1@mail.ru", username="user1")
+    async def update(
+        self, user_id: UUID, data: UserUpdateSchema, users_repo: UserRepository
+    ) -> UserReadSchema:
+        raw_obj = data.model_dump(exclude_unset=True)
+        raw_obj.update({"id": user_id})
+        obj = await users_repo.update(UserModel(**raw_obj))
+        await users_repo.session.commit()
+        return UserReadSchema.model_validate(obj)
 
     @delete("/{user_id:uuid}")
-    async def delete(self, user_id: UUID) -> None:
-        pass
+    async def delete(self, user_id: UUID, users_repo: UserRepository) -> None:
+        await users_repo.delete(user_id)
+        await users_repo.session.commit()
