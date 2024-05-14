e6454f662fe1bde428aeed2e4831e9bb5dd963b5
Mikhail <kozlov_m2@dogma.ru>
2024-05-14 11:12:53 +0300

    cities
    api is implemented

--- src/organization/controllers.py
+++ src/organization/controllers.py
@@ -1,9 +1,10 @@
-from litestar import Controller, get
+from uuid import UUID
+from litestar import Controller, get, post, delete
 from litestar.di import Provide
 from pydantic import TypeAdapter
 
-from organization.models import CityRepository, provide_city_repository
-from organization.scheme import CityReadSchema
+from organization.models import CityModel, CityRepository, provide_city_repository
+from organization.scheme import CityReadSchema, CityCreateSchema, CityUpdateSchema
 
 
 class CityController(Controller):
@@ -16,18 +17,30 @@ class CityController(Controller):
         type_adapter = TypeAdapter(list[CityReadSchema])
         return type_adapter.validate_python(result)
 
-    # @get("/{city_id:uuid}")
-    # async def get(self, city_id: UUID) -> CityReadSchema:
-    #     pass
-    #
-    # @post("/")
-    # async def create(self, data: CityCreateSchema) -> CityReadSchema:
-    #     pass
-    #
-    # @post("/{city_id:uuid}")
-    # async def update(self, city_id: UUID, data: CityUpdateSchema) -> CityReadSchema:
-    #     pass
-    #
-    # @delete("/{city_id:uuid}")
-    # async def delete(self, city_id: UUID) -> None:
-    #     pass
+    @get("/{city_id:uuid}")
+    async def get(self, city_id: UUID, cities_repo: CityRepository) -> CityReadSchema:
+        obj = await cities_repo.get(city_id)
+        return CityReadSchema.model_validate(obj)
+
+    @post("/")
+    async def create(
+        self, data: CityCreateSchema, cities_repo: CityRepository
+    ) -> CityReadSchema:
+        obj = await cities_repo.add(CityModel(**data.model_dump(exclude_unset=True)))
+        await cities_repo.session.commit()
+        return CityReadSchema.model_validate(obj)
+
+    @post("/{city_id:uuid}")
+    async def update(
+        self, city_id: UUID, data: CityUpdateSchema, cities_repo: CityRepository
+    ) -> CityReadSchema:
+        raw_obj = data.model_dump(exclude_unset=True)
+        raw_obj.update({"id": city_id})
+        obj = await cities_repo.update(CityModel(**raw_obj))
+        await cities_repo.session.commit()
+        return CityReadSchema.model_validate(obj)
+
+    @delete("/{city_id:uuid}")
+    async def delete(self, city_id: UUID, cities_repo: CityRepository) -> None:
+        await cities_repo.delete(city_id)
+        await cities_repo.session.commit()
